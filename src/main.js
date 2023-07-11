2c71882ad680f7d9c01a10ab77d290b3fa741484
BennyYe <yeshinebenny@gmail.com>
2023-07-11 14:29:12 +0800

    Install pinia and pinia-plugin-persisredstate

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import pinia from './pinia'
 import router from './router'
 import vuetify from './vuetify'
 
 const app = createApp(App)
 
+app.use(pinia)
 app.use(router)
 app.use(vuetify)
 
