f8c367fcd70bb4c80d0e862774cb04ea06f86767
BennyYe <yeshinebenny@gmail.com>
2023-07-11 16:44:02 +0800

    Install vue-i18n

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import App from './App.vue'
 import pinia from './pinia'
 import router from './router'
 import vuetify from './vuetify'
+import i18n from './i18n'
 
 const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
 app.use(vuetify)
+app.use(i18n)
 
 app.mount('#app')
