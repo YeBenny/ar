a4ede149f3882633e9965a15cffa534384de6dbb
BennyYe <yeshinebenny@gmail.com>
2023-07-13 23:15:16 +0800

    Get event list in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,81 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import router from '@/router'
+import Loading from '../components/Loading.vue'
+import LazyImage from '../components/LazyImage.vue'
+import { getEventList } from '../axios'
 
-const pages = ref([
-  {
-    title: 'ShenZhen',
-    location: 'sz',
-    path: '/image-tracking-mindar-interact'
-  },
-  {
-    title: 'HongKong',
-    location: 'hk',
-    path: '/image-tracking-mindar-interact'
+const overlay = ref(true)
+const events = ref([])
+const isLoading = ref(true)
+const isFinish = ref(true)
+const pageSize = 10
+var startIndex = 1
+
+async function getData() {
+  try {
+    overlay.value = true
+    isLoading.value = true
+    isFinish.value = true
+    startIndex = 1
+    events.value = []
+    let data = await getEventList(startIndex, pageSize)
+    let list = data.result
+    let totalRows = data.pageInfo.totalRows
+    events.value.push(...list)
+    isFinish.value = totalRows === 0 || startIndex + pageSize >= totalRows
+  } catch (err) {
+    console.log(err)
+    isFinish.value = true
+    startIndex = 1
+  } finally {
+    overlay.value = false
+    isLoading.value = false
   }
-])
+}
 
-function onClickPage(page) {
-  router.push({
-    path: page.path,
-    query: {
-      title: page.title,
-      location: page.location
+async function loadMoreData() {
+  try {
+    if (!isLoading.value && !isFinish.value) {
+      isLoading.value = true
+      startIndex = startIndex + pageSize
+      let data = await getEventList(startIndex, pageSize)
+      let list = data.result
+      let totalRows = data.pageInfo.totalRows
+      events.value.push(...list)
+      isFinish.value = totalRows === 0 || startIndex + pageSize >= totalRows
     }
-  })
+  } catch (err) {
+    console.log(err)
+    isFinish.value = false
+    startIndex = startIndex - pageSize
+  } finally {
+    overlay.value = false
+    isLoading.value = false
+  }
+}
+
+getData()
+
+onMounted(() => {
+  window.onscroll = () => {
+    let isBottom =
+      document.documentElement.scrollTop + window.innerHeight ===
+      document.documentElement.offsetHeight
+    if (isBottom) {
+      loadMoreData()
+    }
+  }
+})
+
+function onClickEvent(event) {
+  router.push({
+    path: "/image-tracking-interact",
+    query: {
+      title: event.title,
+      id: event.id
+    }
+  }).then(() => { router.go() })
 }
 </script>
 
@@ -32,10 +85,19 @@ function onClickPage(page) {
     <v-main>
       <v-container>
         <v-row>
-          <template v-for="(page, index) in pages" :key="index">
+          <template v-for="(event, index) in events" :key="index">
             <v-col cols="12">
-              <v-card :value="page" @click="onClickPage(page)">
-                <v-card-title class="text-black">{{ page.title }}</v-card-title>
+              <v-card @click="onClickEvent(event)">
+                <LazyImage v-if="event.image" :src="event.image" />
+                <v-card-title>
+                  {{ event.title }}
+                </v-card-title>
+                <v-card-subtitle>
+                  {{ event.description }}
+                </v-card-subtitle>
+                <v-card-text>
+                  {{ $d(event.startUnixTime * 1000, 'short') }} - {{ $d(event.endUnixTime * 1000, 'short') }}
+                </v-card-text>
               </v-card>
             </v-col>
           </template>
@@ -43,4 +105,6 @@ function onClickPage(page) {
       </v-container>
     </v-main>
   </v-layout>
+
+  <Loading :overlay="overlay" />
 </template>
