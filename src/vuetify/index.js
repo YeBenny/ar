18476c639ad9e9dc66a7e378857abb1fda06b0f5
BennyYe <yeshinebenny@gmail.com>
2023-07-24 23:15:56 +0800

    Update theme

--- src/vuetify/index.js
+++ src/vuetify/index.js
@@ -6,5 +6,26 @@ import * as directives from 'vuetify/directives'
 
 export default createVuetify({
   components,
-  directives
+  directives,
+  theme: {
+    defaultTheme: 'myCustomTheme',
+    themes: {
+      myCustomTheme: {
+        dark: false,
+        colors: {
+          primary: '#03A9F4',
+          'primary-darken-1': '#039BE5',
+          secondary: '#03DAC6',
+          'secondary-darken-1': '#018786',
+          error: '#B00020',
+          warning: '#FB8C00',
+          info: '#2196F3',
+          success: '#4CAF50'
+        },
+        variables: {
+          'activated-opacity': '0.00'
+        }
+      }
+    }
+  }
 })
